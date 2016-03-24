bead7a133d43012c6467a271536c5c5ae62324ea
Rytis Bizauskas <luxcapto@gmail.com>
2016-03-23 20:27:08 -0500

    More home twig testing

--- web/index.php
+++ web/index.php
@@ -20,7 +20,7 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
 $app->get('/', function() use($app) {
   $app['monolog']->addDebug('logging output.');
   //return $app['twig']->render('index.twig');
-	return $app['twig']->render('index.twig');
+	return $app['twig']->render('home.twig');
 });
 
 $app->run();
