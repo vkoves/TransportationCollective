045580dff9935c97dcb96e25ffff55900a4aa964
Viktor Koves <viktorkoves@gmail.com>
2016-04-06 19:42:24 -0500

    Added cookie test

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
+var userStops = [];
+
 $(document).ready(function()
 {
-
+	//Cookies tutorial http://www.w3schools.com/js/js_cookies.asp
+	document.cookie = "favoriteStops=Red:Sox-35th,Blue:Irving Park,Green:35th-Bronzeville-IIT"
+	console.log("Set cookies! \"" + document.cookie + "\"");
 });
