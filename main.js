6eaae63316bb2397a4b2e136087ada2381183d0d
Viktor Koves <viktorkoves@gmail.com>
2016-04-06 19:45:44 -0500

    Added prototype favorite stop functionality

--- main.js
+++ main.js
@@ -3,6 +3,17 @@ var userStops = [];
 $(document).ready(function()
 {
 	//Cookies tutorial http://www.w3schools.com/js/js_cookies.asp
-	document.cookie = "favoriteStops=Red:Sox-35th,Blue:Irving Park,Green:35th-Bronzeville-IIT"
+	document.cookie = "favoriteStops=Red:Sox-35th,Blue:Irving Park,Green:35th-Bronzeville-IIT,"
 	console.log("Set cookies! \"" + document.cookie + "\"");
+
+	showFavoriteStops();
 });
+
+function showFavoriteStops()
+{
+	var arr = document.cookie.split("favoriteStops=");
+	var stopString = arr[arr.length];
+	var stopArray = stopString.split(",");
+	console.log("Stop arr: ");
+	console.log(stopArray);
+}
