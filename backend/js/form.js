f9017cf3b1cc6272e3c5c3589cf4fa30737ee675
Potatopat <potatopat1973@gmail.com>
2016-04-13 18:15:58 -0500

    come-on query

--- backend/js/form.js
+++ backend/js/form.js
@@ -19,11 +19,33 @@ function drawChart() {
 	
 	
 	
-	var query = new google.visualization.Query(
+	//var query = new google.visualization.Query(
         //'http://spreadsheets.google.com/tq?key=pCQbetd-CptGXxxQIG7VFIQ&pub=1');
         //'https://docs.google.com/spreadsheets/d/1oNIORrgb9beapo4S6AiRAwBZrEQ3U-OwYROQvPKnzdI/edit#gid=1575241258'
         //'https://spreadsheets.google.com/tq?key=1oNIORrgb9beapo4S6AiRAwBZrEQ3U-OwYROQvPKnzdI&pub=1');
-        'https://docs.google.com/spreadsheets/d/1oNIORrgb9beapo4S6AiRAwBZrEQ3U-OwYROQvPKnzdI/edit?usp=sharing');
+    //    'https://docs.google.com/spreadsheets/d/1oNIORrgb9beapo4S6AiRAwBZrEQ3U-OwYROQvPKnzdI/edit?usp=sharing');
         
-    console.log(query);
-}
+    //console.log(query);
+//}
+
+//function drawVisualization() {
+    var query = new google.visualization.Query(
+        'http://spreadsheets.google.com/tq?key=pCQbetd-CptGXxxQIG7VFIQ&pub=1');
+
+    // Apply query language statement.
+    query.setQuery('SELECT A,D WHERE D > 100 ORDER BY D');
+    
+    // Send the query with a callback function.
+    query.send(handleQueryResponse);
+  }
+
+  function handleQueryResponse(response) {
+    if (response.isError()) {
+      alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+      return;
+    }
+
+    var data = response.getDataTable();
+    visualization = new google.visualization.LineChart(document.getElementById('visualization'));
+    visualization.draw(data, {legend: 'bottom'});
+  }
