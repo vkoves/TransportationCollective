302386a39b95a77e7eef7b89802aded7fb58cd27
Potatopat <potatopat1973@gmail.com>
2016-04-13 18:10:52 -0500

    Query?

--- backend/js/form.js
+++ backend/js/form.js
@@ -1,17 +1,28 @@
 //		https://spreadsheets.google.com/tq?&tq=&key=1oNIORrgb9beapo4S6AiRAwBZrEQ3U-OwYROQvPKnzdI&gid=2
 
 function drawChart() {
-      // Define the chart to be drawn.
-      var data = new google.visualization.DataTable();
-      data.addColumn('string', 'Element');
-      data.addColumn('number', 'Percentage');
-      data.addRows([
-        ['Nitrogen', 0.78],
-        ['Oxygen', 0.21],
-        ['Other', 0.01]
-      ]);
-
-      // Instantiate and draw the chart.
-      var chart = new google.visualization.PieChart(document.getElementById('myPieChart'));
-      chart.draw(data, null);
-    }
+	// Define the chart to be drawn.
+	/*
+	var data = new google.visualization.DataTable();
+	data.addColumn('string', 'Element');
+	data.addColumn('number', 'Percentage');
+	data.addRows([
+		['Nitrogen', 0.78],
+		['Oxygen', 0.21],
+		['Other', 0.01]
+	]);
+	
+	// Instantiate and draw the chart.
+	var chart = new google.visualization.PieChart(document.getElementById('myPieChart'));
+	chart.draw(data, null);
+	*/
+	
+	
+	
+	var query = new google.visualization.Query(
+        //'http://spreadsheets.google.com/tq?key=pCQbetd-CptGXxxQIG7VFIQ&pub=1');
+        //'https://docs.google.com/spreadsheets/d/1oNIORrgb9beapo4S6AiRAwBZrEQ3U-OwYROQvPKnzdI/edit#gid=1575241258'
+        'https://spreadsheets.google.com/tq?key=1oNIORrgb9beapo4S6AiRAwBZrEQ3U-OwYROQvPKnzdI&pub=1');
+        
+    console.log(query);
+}
