806fc84abcf09cd172296e22341490352bb69001
Potatopat <potatopat1973@gmail.com>
2016-04-13 17:59:13 -0500

    Chart test 1

--- backend/js/form.js
+++ backend/js/form.js
@@ -1,1 +1,17 @@
 //		https://spreadsheets.google.com/tq?&tq=&key=1oNIORrgb9beapo4S6AiRAwBZrEQ3U-OwYROQvPKnzdI&gid=2
+
+function drawChart() {
+      // Define the chart to be drawn.
+      var data = new google.visualization.DataTable();
+      data.addColumn('string', 'Element');
+      data.addColumn('number', 'Percentage');
+      data.addRows([
+        ['Nitrogen', 0.78],
+        ['Oxygen', 0.21],
+        ['Other', 0.01]
+      ]);
+
+      // Instantiate and draw the chart.
+      var chart = new google.visualization.PieChart(document.getElementById('myPieChart'));
+      chart.draw(data, null);
+    }
