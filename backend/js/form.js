0f6904fc23fd630cfde8df4a61c5f50f0157439f
Henry Winn <henrywinn@henrywinn.com>
2016-04-13 19:43:42 -0500

    Gets data into pie chart Refactors code

--- backend/js/form.js
+++ backend/js/form.js
@@ -1,19 +1,24 @@
-function lineIssues() {
-	var queryString = encodeURIComponent('SELECT D');
-	var query = new google.visualization.Query(
-          'https://docs.google.com/spreadsheets/d/1oNIORrgb9beapo4S6AiRAwBZrEQ3U-OwYROQvPKnzdI/gviz/tq?gid=1575241258&headers=1&tq=' + queryString);
-    query.send(lineIssuesQuery);
+function drawCharts() {
+    pieIssuePerLine();
 }
-function lineIssuesQuery(response) {
-	if (response.isError()) {
-		alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
-		return;
+
+function pieIssuePerLine(response) {
+    if(typeof(response) == 'undefined') {
+        var queryString = encodeURIComponent('SELECT D, COUNT(A) GROUP BY D');
+        var query = new google.visualization.Query(
+            'https://docs.google.com/spreadsheets/d/1oNIORrgb9beapo4S6AiRAwBZrEQ3U-OwYROQvPKnzdI/gviz/tq?gid=1575241258&headers=1&tq=' + queryString);
+        query.send(pieIssuePerLine);
     }
-    
-    var data = response.getDataTable();
-    
-    console.log(data);
-    
-    var chart = new google.visualization.Table(document.getElementById('myPieChart'));
-	chart.draw(data, null);
+    else {
+        if (response.isError()) {
+            alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+            return;
+        }
+        var data = response.getDataTable();
+
+        console.log(data);
+
+        var chart = new google.visualization.PieChart(document.getElementById('myPieChart'));
+        chart.draw(data, null);
+    }
 }
