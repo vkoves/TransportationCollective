38a56a639f6d8297204bc76bcbfc753a2753fc65
Henry Winn <henrywinn@henrywinn.com>
2016-04-13 20:17:11 -0500

    Completes function that draws table of issues for a specific line and stop

--- backend/js/form.js
+++ backend/js/form.js
@@ -1,5 +1,6 @@
 function drawCharts() {
     pieIssuePerLine();
+    recentIssues("Green Line","Roosevelt",document.getElementById('wow'));
 }
 
 function pieIssuePerLine(response) {
@@ -21,4 +22,23 @@ function pieIssuePerLine(response) {
         var chart = new google.visualization.PieChart(document.getElementById('myPieChart'));
         chart.draw(data, null);
     }
+}
+
+/**
+ *
+ * @param line String which line are we talking about
+ * @param stop String which stop
+ * @param where Element document element where table will be drawn
+ */
+function recentIssues(line, stop, where) {
+    var queryString = encodeURIComponent('SELECT F,G WHERE D = "'+line+'" AND E = "'+stop+'"');
+    var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1oNIORrgb9beapo4S6AiRAwBZrEQ3U-OwYROQvPKnzdI/gviz/tq?gid=1575241258&headers=1&tq=' + queryString);
+    query.send(function(response){
+        var data = response.getDataTable();
+
+        console.log(data);
+
+        var chart = new google.visualization.Table(where);
+        chart.draw(data, null);
+    });
 }
